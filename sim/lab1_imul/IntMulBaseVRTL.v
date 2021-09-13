8119542a01b014ec0605802a1104b7cb3724a19f
ber72 <ber72@cornell.edu>
2021-09-13 12:20:59 -0400

    Began Control FSM for IntMulBaseVRTL

--- sim/lab1_imul/IntMulBaseVRTL.v
+++ sim/lab1_imul/IntMulBaseVRTL.v
@@ -34,6 +34,102 @@ module lab1_imul_IntMulBaseVRTL
   // together.
   // '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
+  //CONTROL DESIGN START
+
+module lab1_imul_IntMulBaseVRTL_IntMulBaseCtrl(
+  
+  input  logic        clk,
+  input  logic        reset,
+
+  // Dataflow signals
+
+  input  logic        req_val,
+  output logic        req_rdy,
+  output logic        resp_val,
+  input  logic        resp_rdy,
+
+  // Control Signals
+
+  output logic a_mux_sel,
+  output logic b_mux_sel,
+  output logic add_mux_sel,
+  output logic result_mux_sel,
+  output logic result_en,
+
+  // Data Signals
+  
+  input logic b_lsb,
+
+  // Mux Parameters
+
+  localparam b_rs = 1'd0;
+  localparam b_ld = 1'd1;
+  
+  localparam a_ls = 1'd0;
+  localparam a_ld = 1'd1;
+  
+  localparam add_add = 1'd0;
+  localparam add_resp = 1'd1;
+
+  localparam result_add = 1'd0;
+  localparam result_0 = 1'd1;
+
+);
+
+  //----------------------------------------------------------------------
+  // State Definitions
+  //----------------------------------------------------------------------
+
+  localparam STATE_IDLE = 2'd0;
+  localparam STATE_CALC = 2'd1;
+  localparam STATE_DONE = 2'd2;
+
+  //----------------------------------------------------------------------
+  // State
+  //----------------------------------------------------------------------
+
+  logic [1:0] state_reg;
+  logic [1:0] state_next;
+
+  always_ff @( posedge clk ) begin
+    if ( reset ) begin
+      state_reg <= STATE_IDLE;
+    end
+    else begin
+      state_reg <= state_next;
+    end
+  end
+
+  //----------------------------------------------------------------------
+  // State Transitions
+  //----------------------------------------------------------------------
+
+  logic req_go;
+  logic resp_go;
+  logic is_calc_done;
+
+  assign req_go       = req_val  && req_rdy;
+  assign resp_go      = resp_val && resp_rdy;
+  assign is_calc_done = b_lsb && is_b_zero;
+
+  always_comb begin
+
+    state_next = state_reg;
+
+    case ( state_reg )
+
+      STATE_IDLE: if ( req_go    )    state_next = STATE_CALC;
+      STATE_CALC: if ( is_calc_done ) state_next = STATE_DONE;
+      STATE_DONE: if ( resp_go   )    state_next = STATE_IDLE;
+      default:    state_next = 'x;
+
+    endcase
+
+  end
+
+
+  //CONTROL DESIGN END
+
   //----------------------------------------------------------------------
   // Line Tracing
   //----------------------------------------------------------------------
