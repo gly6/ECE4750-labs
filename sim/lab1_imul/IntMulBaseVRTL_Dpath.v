ef9d81d1b8781fdc0f2dfc97c4d18115ad959bf7
gly6 <gly6@cornell.edu>
2021-09-14 16:59:09 -0400

    fix bit bug

--- sim/lab1_imul/IntMulBaseVRTL_Dpath.v
+++ sim/lab1_imul/IntMulBaseVRTL_Dpath.v
@@ -8,13 +8,13 @@ module lab1_imul_IntMulBaseVRTL_Dpath(
   input  logic        reset,
   
   //Data Signal 
-  input  logic        req_val,
+  //input  logic        req_val,
   //output logic        req_rdy,
-  //input  logic [63:0] req_msg,
+  input  logic [63:0] req_msg,
 
-  output logic        resp_val,
+  //output logic        resp_val,
   //input  logic        resp_rdy,
-  //output logic [31:0] resp_msg,
+  output logic [31:0] resp_msg,
 
   //Control Signal
   input  logic        a_mux_sel,
@@ -93,7 +93,7 @@ logic [c_nbits-1:0] result_mux_out;
 vc_Mux2#(c_nbits) result_mux 
 (
   .in0  (add_mux_out),
-  .in1  (0'b1), 
+  .in1  (0), 
   .sel  (result_mux_sel),
   .out  (result_mux_out)
 );
