7288a47e1323dd2985f909e294b857e53f7392f8
kae87 <kae87@cornell.edu>
2021-09-14 16:31:17 -0400

    Bugfixes in Dpath

--- sim/lab1_imul/IntMulBaseVRTL_Dpath.v
+++ sim/lab1_imul/IntMulBaseVRTL_Dpath.v
@@ -9,12 +9,12 @@ module lab1_imul_IntMulBaseVRTL_Dpath(
   
   //Data Signal 
   input  logic        req_val,
-  //output logic        req_rdy,
-  //input  logic [63:0] req_msg,
+  output logic        req_rdy,
+  input  logic [63:0] req_msg,
 
   output logic        resp_val,
-  //input  logic        resp_rdy,
-  //output logic [31:0] resp_msg,
+  input  logic        resp_rdy,
+  output logic [31:0] resp_msg,
 
   //Control Signal
   input  logic        a_mux_sel,
