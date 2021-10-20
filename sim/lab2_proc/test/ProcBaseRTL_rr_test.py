a44af162a211763c1eafd8f5c142b5ca508e5a94
kae87 <kae87@cornell.edu>
2021-10-20 17:13:17 -0400

    added more tests

--- sim/lab2_proc/test/ProcBaseRTL_rr_test.py
+++ sim/lab2_proc/test/ProcBaseRTL_rr_test.py
@@ -186,18 +186,22 @@ import inst_sltu
 
 @pytest.mark.parametrize( "name,test", [
   asm_test( inst_sltu.gen_basic_test     ) ,
-
-  # ''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-  # Add more rows to the test case table to test more complicated
-  # scenarios.
-  # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  asm_test( inst_sltu.gen_dest_dep_test  ) ,
+  asm_test( inst_sltu.gen_src0_dep_test  ) ,
+  asm_test( inst_sltu.gen_src1_dep_test  ) ,
+  asm_test( inst_sltu.gen_srcs_dep_test  ) ,
+  asm_test( inst_sltu.gen_srcs_dest_test ) ,
+  asm_test( inst_sltu.gen_value_test     ) ,
+  asm_test( inst_sltu.gen_random_test    ) ,
 ])
+
 def test_sltu( name, test, dump_vcd ):
   run_test( ProcBaseRTL, test, dump_vcd )
 
-# ''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-# random stall and delay
-# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def test_sltu_rand_delays( dump_vcd ):
+  run_test( ProcBaseRTL, inst_sltu.gen_random_test, dump_vcd,
+            src_delay=3, sink_delay=5, mem_stall_prob=0.5, mem_latency=3 )
+
 #-------------------------------------------------------------------------
 # sra
 #-------------------------------------------------------------------------
@@ -206,18 +210,22 @@ import inst_sra
 
 @pytest.mark.parametrize( "name,test", [
   asm_test( inst_sra.gen_basic_test     ) ,
-
-  # ''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-  # Add more rows to the test case table to test more complicated
-  # scenarios.
-  # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  asm_test( inst_sra.gen_dest_dep_test  ) ,
+  asm_test( inst_sra.gen_src0_dep_test  ) ,
+  asm_test( inst_sra.gen_src1_dep_test  ) ,
+  asm_test( inst_sra.gen_srcs_dep_test  ) ,
+  asm_test( inst_sra.gen_srcs_dest_test ) ,
+  asm_test( inst_sra.gen_value_test     ) ,
+  asm_test( inst_sra.gen_random_test    ) ,
 ])
+
 def test_sra( name, test, dump_vcd ):
   run_test( ProcBaseRTL, test, dump_vcd )
 
-# ''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-# random stall and delay
-# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def test_sra_rand_delays( dump_vcd ):
+  run_test( ProcBaseRTL, inst_sra.gen_random_test, dump_vcd,
+            src_delay=3, sink_delay=5, mem_stall_prob=0.5, mem_latency=3 )
+
 #-------------------------------------------------------------------------
 # srl
 #-------------------------------------------------------------------------
@@ -226,18 +234,22 @@ import inst_srl
 
 @pytest.mark.parametrize( "name,test", [
   asm_test( inst_srl.gen_basic_test     ) ,
-
-  # ''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-  # Add more rows to the test case table to test more complicated
-  # scenarios.
-  # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  asm_test( inst_srl.gen_dest_dep_test  ) ,
+  asm_test( inst_srl.gen_src0_dep_test  ) ,
+  asm_test( inst_srl.gen_src1_dep_test  ) ,
+  asm_test( inst_srl.gen_srcs_dep_test  ) ,
+  asm_test( inst_srl.gen_srcs_dest_test ) ,
+  asm_test( inst_srl.gen_value_test     ) ,
+  asm_test( inst_srl.gen_random_test    ) ,
 ])
+
 def test_srl( name, test, dump_vcd ):
   run_test( ProcBaseRTL, test, dump_vcd )
 
-# ''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-# random stall and delay
-# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def test_srl_rand_delays( dump_vcd ):
+  run_test( ProcBaseRTL, inst_srl.gen_random_test, dump_vcd,
+            src_delay=3, sink_delay=5, mem_stall_prob=0.5, mem_latency=3 )
+
 #-------------------------------------------------------------------------
 # sll
 #-------------------------------------------------------------------------
@@ -246,17 +258,18 @@ import inst_sll
 
 @pytest.mark.parametrize( "name,test", [
   asm_test( inst_sll.gen_basic_test     ) ,
-
-  # ''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-  # Add more rows to the test case table to test more complicated
-  # scenarios.
-  # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  asm_test( inst_sll.gen_dest_dep_test  ) ,
+  asm_test( inst_sll.gen_src0_dep_test  ) ,
+  asm_test( inst_sll.gen_src1_dep_test  ) ,
+  asm_test( inst_sll.gen_srcs_dep_test  ) ,
+  asm_test( inst_sll.gen_srcs_dest_test ) ,
+  asm_test( inst_sll.gen_value_test     ) ,
+  asm_test( inst_sll.gen_random_test    ) ,
 ])
+
 def test_sll( name, test, dump_vcd ):
   run_test( ProcBaseRTL, test, dump_vcd )
 
-# ''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-# random stall and delay
-# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-
-
+def test_sll_rand_delays( dump_vcd ):
+  run_test( ProcBaseRTL, inst_sll.gen_random_test, dump_vcd,
+            src_delay=3, sink_delay=5, mem_stall_prob=0.5, mem_latency=3 )
