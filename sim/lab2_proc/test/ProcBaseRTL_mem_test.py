1155225106cce225367846d253c71627dfb70136
gly6 <gly6@cornell.edu>
2021-10-23 11:34:29 -0400

    add the sw test

--- sim/lab2_proc/test/ProcBaseRTL_mem_test.py
+++ sim/lab2_proc/test/ProcBaseRTL_mem_test.py
@@ -38,7 +38,11 @@ import inst_sw
 
 @pytest.mark.parametrize( "name,test", [
   asm_test( inst_sw.gen_basic_test     ),
-
+  asm_test( inst_sw.gen_dest_dep_test  ) ,
+  asm_test( inst_sw.gen_base_dep_test  ) ,
+  asm_test( inst_sw.gen_srcs_dest_test ) ,
+  asm_test( inst_sw.gen_value_test     ) ,
+  asm_test( inst_sw.gen_random_test    ) ,
   # ''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   # Add more rows to the test case table to test more complicated
   # scenarios.
@@ -46,6 +50,10 @@ import inst_sw
 ])
 def test_sw( name, test, dump_vcd ):
   run_test( ProcBaseRTL, test, dump_vcd )
+
+def test_sw_rand_delays( dump_vcd ):
+  run_test( ProcBaseRTL, inst_sw.gen_random_test, dump_vcd,
+            src_delay=3, sink_delay=5, mem_stall_prob=0.5, mem_latency=3 )
 
 # ''' LAB TASK '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 # random stall and delay
