b96d115c5633569a5a365a579f0714ee0e183189
gly6 <gly6@cornell.edu>
2021-11-09 20:24:44 -0500

    some early test cases

--- sim/lab3_mem/test/BlockingCacheFL_test.py
+++ sim/lab3_mem/test/BlockingCacheFL_test.py
@@ -116,7 +116,9 @@ def resp( type_, opaque, test, len, data ):
   msg.test   = test
   msg.data   = data
   return msg
-
+#----------------------------------------------------------------------
+# Given Test Cases 
+#----------------------------------------------------------------------
 #----------------------------------------------------------------------
 # Test Case: read hit path
 #----------------------------------------------------------------------
@@ -191,9 +193,81 @@ def read_miss_1word_mem( base_addr ):
   ]
 
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-# LAB TASK: Add more test cases
+# Created Test Cases 
 #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Read hit path for clean lines
+#'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def read_hit_clean (base_addr): 
+  return [
+    #    type  opq   addr      len  data               type  opq test len  data
+    req( 'in', 0x00, 0x00000000, 0, 0xdeadbeef ), resp('in', 0x00, 0, 0,  0       ),
+    req( 'rd', 0x01, 0x00000000, 0, 0          ), resp('rd', 0x01, 1, 0, 0xdeadbeef ), # read word  0x00000000
+    req( 'rd', 0x02, 0x00000004, 0, 0          ), resp('rd', 0x02, 1, 0, 0          ), # read word  0x00000004
+    req( 'rd', 0x03, 0x00000008, 0, 0          ), resp('rd', 0x03, 1, 0, 0          ), # read word  0x00000008
+    req( 'rd', 0x04, 0x0000000c, 0, 0          ), resp('rd', 0x04, 1, 0, 0          ), # read word  0x0000000c
+  ]
+#'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Write hit path for clean lines
+#'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+def write_hit_clean (base_addr): 
+  return [
+    #    type  opq   addr      len  data               type  opq test len  data
+    req( 'in', 0x00, 0x00000000, 0, 0xdeadbeef ), resp('in', 0x00, 0, 0, 0        ),
+    req( 'wr', 0x01, 0x00000004, 0, 0x00c0ffee ), resp('in', 0x01, 0, 0, 0        ),
+    req( 'rd', 0x02, 0x00000000, 0, 0          ), resp('rd', 0x02, 1, 0, 0xdeadbeef ), # read word  0x00000000
+    req( 'rd', 0x03, 0x00000004, 0, 0          ), resp('rd', 0x03, 1, 0, 0x00c0ffee ), # read word  0x00000004
+    req( 'rd', 0x04, 0x00000008, 0, 0          ), resp('rd', 0x04, 1, 0, 0          ), # read word  0x00000008
+    req( 'rd', 0x05, 0x0000000c, 0, 0          ), resp('rd', 0x05, 1, 0, 0          ), # read word  0x0000000c
+  ]
 
+#'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Read miss with refill and no eviction
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''' 
+def read_miss_wr_woe( base_addr ):
+  return [
+    #    type  opq   addr      len  data               type  opq test len  data
+    req( 'rd', 0x00, 0x00000000, 0, 0          ), resp('rd', 0x00, 1, 0, 0xdeadbeef ), # read word  0x00000000
+    req( 'rd', 0x01, 0x00000004, 0, 0          ), resp('rd', 0x01, 1, 0, 0x00c0ffee ), # read word  0x00000004
+    req( 'rd', 0x02, 0x00000008, 0, 0          ), resp('rd', 0x02, 1, 0, 0xabcdefab ), # read word  0x00000008
+    req( 'rd', 0x03, 0x0000000c, 0, 0          ), resp('rd', 0x03, 1, 0, 0x01234567 ), # read word  0x0000000c
+    req( 'rd', 0x04, 0x00000010, 0, 0          ), resp('rd', 0x04, 0, 0, 0xdededede ), # read word  0x00000010
+  ]
+
+# Data to be loaded into memory before running the test
+
+def read_miss_wr_woe_mem( base_addr ):
+  return [
+    # addr      data (in int)
+    0x00000000, 0xdeadbeef,
+    0x00000004, 0x00c0ffee,
+    0x00000008, 0xabcdefab,
+    0x0000000c, 0x01234567, 
+    0x00000010, 0xdededede,   
+  ]
+#'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+# Write miss with refill and no eviction
+#''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''' 
+def write_miss_wr_woe (base_addr) : 
+  return [
+    #    type  opq   addr      len  data               type  opq test len  data
+    req( 'wr', 0x00, 0x00000000, 0, 0xdeadbeef ), resp('wr', 0x00, 0, 0, 0x0), # write word  0x00000000
+    req( 'rd', 0x01, 0x00000000, 0, 0          ), resp('rd', 0x01, 1, 0, 0xdeadbeef ), # read word   0x00000000
+    req( 'rd', 0x02, 0x00000004, 0, 0          ), resp('rd', 0x02, 1, 0, 0x00c0ffee ), # read word  0x00000004
+    req( 'rd', 0x03, 0x00000008, 0, 0          ), resp('rd', 0x03, 1, 0, 0xabcdefab ), # read word  0x00000008
+    req( 'rd', 0x04, 0x0000000c, 0, 0          ), resp('rd', 0x04, 1, 0, 0x01234567 ), # read word  0x0000000c
+  ]
+
+# Data to be loaded into memory before running the test
+def write_miss_wr_woe_mem ( base_addr ):
+  return [
+    # addr      data (in int)
+    0x00000000, 0x00000000,
+    0x00000004, 0x00c0ffee,
+    0x00000008, 0xabcdefab,
+    0x0000000c, 0x01234567, 
+    0x00000010, 0x00000001,   
+  ]
 #----------------------------------------------------------------------
 # Banked cache test
 #----------------------------------------------------------------------
@@ -215,9 +289,14 @@ def read_miss_1word_mem( base_addr ):
 
 test_case_table_generic = mk_test_case_table([
   (                         "msg_func               mem_data_func         nbank stall lat src sink"),
+  #given test cases 
   [ "read_hit_1word_clean",  read_hit_1word_clean,  None,                 0,    0.0,  0,  0,  0    ],
   [ "read_miss_1word",       read_miss_1word_msg,   read_miss_1word_mem,  0,    0.0,  0,  0,  0    ],
   [ "read_hit_1word_4bank",  read_hit_1word_clean,  None,                 4,    0.0,  0,  0,  0    ],
+  [ "read_hit_clean",        read_hit_clean,        None,                 0,    0.0,  0,  0,  0    ],
+  [ "write_hit_clean",       write_hit_clean,       None,                 0,    0.0,  0,  0,  0    ],
+  [ "read_miss_wr_woe",      read_miss_wr_woe,      read_miss_wr_woe_mem, 0,    0.0,  0,  0,  0    ],
+  [ "write_miss_wr_woe",     write_miss_wr_woe,     write_miss_wr_woe_mem,0,    0.0,  0,  0,  0    ],
 
   #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   # LAB TASK: Add test cases to this table
@@ -281,7 +360,6 @@ def test_set_assoc( test_params, dump_vcd ):
 test_case_table_dir_mapped = mk_test_case_table([
   (                                  "msg_func              mem_data_func          nbank stall lat src sink"),
   [ "read_hit_dmap",                  read_hit_dmap,        None,                  0,    0.0,  0,  0,  0    ],
-
   #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
   # LAB TASK: Add test cases to this table
   #'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
